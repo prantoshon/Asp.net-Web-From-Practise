01b65cac28bcfa6a81549c78039a3189ccabe2f9
Pranto <prantoshon14@gmail.com>
2020-05-14 00:23:15 +0600

    Add files via upload
    export to excel Button from grideview

---
+++ Serach.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+namespace Registration
+{
+    public partial class Serach : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-HM3JBK0\\SQLEXPRESS;Initial Catalog=PPL;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+            if (!IsPostBack)
+            {
+                Panel1.Visible = true;
+                DisplayData();
+            }
+        }
+        private void DisplayData()
+        {
+            
+            SqlDataAdapter Adp = new SqlDataAdapter("SELECT  * FROM   hrd.Department", con);
+            DataTable Dt = new DataTable();
+            Adp.Fill(Dt);
+            GridView1.DataSource = Dt;
+            GridView1.DataBind();
+           //  return Dt;
+        }
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            String str = "select * from hrd.Department where (Code like '%' + @search + '%')";
+            SqlCommand xp = new SqlCommand(str, con);
+            xp.Parameters.Add("@search", SqlDbType.NVarChar).Value = TextBox1.Text;
+            con.Open();
+            xp.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = xp;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Code");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+            if (GridView1.Rows.Count == 0)
+            {
+                Response.Write("<script>alert('Record Not Founded');</script>");
+                TextBox1.Focus();
+                DisplayData();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String str = "select * from hrd.Department where (Code like '%' + @search + '%')";
+            SqlCommand xp = new SqlCommand(str, con);
+            xp.Parameters.Add("@search", SqlDbType.NVarChar).Value = TextBox1.Text;
+            con.Open();
+            xp.ExecuteNonQuery();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = xp;
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Code");
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+            con.Close();
+          
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            string Code = GridView1.DataKeys[e.RowIndex].Values["Code"].ToString();
+            string txt2 = ((TextBox)row.Cells[1].Controls[0]).Text;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE hrd.Department set Code='" + txt2 + "' where Code=" + Code, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            GridView1.DataBind();
+        }
+
+        protected void Unnamed1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_Click1(object sender, EventArgs e)
+        {
+
+           
+        }
+        public override void VerifyRenderingInServerForm(Control control)
+        {
+        }
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            // Clear all content output from the buffer stream
+            Response.ClearContent();
+            // Specify the default file name using "content-disposition" RESPONSE header
+            Response.AppendHeader("content-disposition", "attachment; filename=Employees.xls");
+            // Set excel as the HTTP MIME type
+            Response.ContentType = "application/excel";
+            // Create an instance of stringWriter for writing information to a string
+            System.IO.StringWriter stringWriter = new System.IO.StringWriter();
+            // Create an instance of HtmlTextWriter class for writing markup 
+            // characters and text to an ASP.NET server control output stream
+            HtmlTextWriter htw = new HtmlTextWriter(stringWriter);
+
+            // Set white color as the background color for gridview header row
+            GridView1.HeaderRow.Style.Add("background-color", "#FFFFFF");
+
+            // Set background color of each cell of GridView1 header row
+            foreach (TableCell tableCell in GridView1.HeaderRow.Cells)
+            {
+                tableCell.Style["background-color"] = "#A55129";
+            }
+
+            // Set background color of each cell of each data row of GridView1
+            foreach (GridViewRow gridViewRow in GridView1.Rows)
+            {
+                gridViewRow.BackColor = System.Drawing.Color.White;
+                foreach (TableCell gridViewRowTableCell in gridViewRow.Cells)
+                {
+                    gridViewRowTableCell.Style["background-color"] = "#FFF7E7";
+                }
+            }
+
+            GridView1.RenderControl(htw);
+            Response.Write(stringWriter.ToString());
+            Response.End();
+        }
+    }
+}
